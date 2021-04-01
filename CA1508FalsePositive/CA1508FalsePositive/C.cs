55ef3acd011164a45651311b9feaeab41588e630
Nathan Moreau <nathan.moreau@m4x.org>
2021-04-01 18:26:51 +0200

    CA1508: reports false positive in the generic case.

---
+++ CA1508FalsePositive/CA1508FalsePositive/C.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace CA1508FalsePositive
+{
+    public static class Class1
+    {
+        public static int F<T>(IEnumerable<T> sequence)
+        {
+            foreach (var value in sequence ?? System.Array.Empty<T>())
+            {
+                int? hash = value?.GetHashCode();
+                return hash ?? 0;
+            }
+            return -1;
+        }
+
+        public static int G(IEnumerable<int?> sequence)
+        {
+            foreach (var value in sequence ?? System.Array.Empty<int?>())
+            {
+                int? hash = value?.GetHashCode();
+                return hash ?? 0;
+            }
+            return -1;
+        }
+    }
+}
